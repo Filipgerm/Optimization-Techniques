ef930682e07283b45f37458b48e0f01e47e390fd
Filippos Germanopoulos <108792752+Filipgerm@users.noreply.github.com>
2023-04-04 10:36:41 +0300

    Delete Bisection_with_Derivative directory

--- Bisection_with_Derivative/Find_n.m
+++
@@ -1,8 +0,0 @@
-function [n] = Find_n(ak, bk, l)
-%Calculates the minimum n condition, which serves the Bisection Method with the use of its derivative.
-n = 1;
-    while (1/2)^n >=(l/(bk-ak))
-    n = n + 1;
-    end
-end
-

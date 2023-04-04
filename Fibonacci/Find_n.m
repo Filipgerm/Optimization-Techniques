685b9e759bb22e72c7a627ccfe8f6b065affd215
Filippos Germanopoulos <108792752+Filipgerm@users.noreply.github.com>
2023-04-04 10:36:35 +0300

    Delete Fibonacci directory

--- Fibonacci/Find_n.m
+++
@@ -1,8 +0,0 @@
-function [n] = Find_n(ak, bk, l)
-%Calculates the minimum n condition, which serves the FIbonacci Algorithm.
-n = 1;
-    while (fibonacci(n))<((bk-ak)/l)
-    n = n + 1;
-    end
-end
-
